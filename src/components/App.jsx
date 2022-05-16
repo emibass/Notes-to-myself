a907d49014dd78d511c107bbfd72ba337c15ab7d
Emilia Fuks <emilia.lippa@gmail.com>
2022-05-17 10:20:37 +1200

    add local storage with useEffect hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,6 +6,17 @@ import CreateArea from "./CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    if (notes) {
+      setNotes(notes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   function addNote(newNote) {
     setNotes((prevNotes) => {
